34192aa6aef2c64c1f5a2ad795eec649dc15948f
laurauzcategui <laura.uzcateguij@gmail.com>
2017-10-31 22:33:53 +0000

    Inspect decorator + mock of timeout and memoized

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,1 +1,21 @@
 # implement your decorators here.
+import functools
+
+def inspect(fn):
+    @functools.wraps(fn)
+    def wrapper(a,b):
+        print("{} invoked with {} {}".format(fn.__name__, a, b))
+        fn(a,b)
+    return wrapper
+
+@inspect
+def my_add(a, b):
+    return a + b
+
+my_add(3, 5)
+
+def timeout(fn):
+    pass
+
+def memoized(fn):
+    pass
