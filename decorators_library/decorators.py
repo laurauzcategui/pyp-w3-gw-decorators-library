a0436f79cf22537239f1e344f31934822e26cf27
laurauzcategui <laura.uzcateguij@gmail.com>
2017-11-01 22:55:11 +0000

    fixing inspect, end timeout

--- decorators_library/decorators.py
+++ decorators_library/decorators.py
@@ -1,21 +1,51 @@
 # implement your decorators here.
 import functools
+import signal
+import time
+from .exceptions import FunctionTimeoutException
+from .exceptions import MyCoolException
 
 def inspect(fn):
-    @functools.wraps(fn)
-    def wrapper(a,b):
-        print("{} invoked with {} {}".format(fn.__name__, a, b))
-        fn(a,b)
+    #@functools.wraps(fn)
+    def wrapper(*args,**kwargs):
+        list_args = [str(arg) for arg in args]
+        func_name = fn.__name__
+        result = fn(*args,**kwargs)
+        print("{} invoked with {}. Result: {}").format(func_name, ','.join(list_args), result)
+        return fn(*args,**kwargs)
     return wrapper
 
-@inspect
-def my_add(a, b):
-    return a + b
 
-my_add(3, 5)
+class timeout(object):
+    def __init__(self, alarm, exception=FunctionTimeoutException):
+        self.alarm = alarm
+        self.exception = exception
 
-def timeout(fn):
-    pass
+    def __call__(self, fn, *args, **kwargs):
+
+        def receive_alarm(signum, stack):
+            time.ctime()
+            raise self.exception("Function call timed out")
+
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, receive_alarm)
+            signal.alarm(self.alarm)
+
+            return fn(*args, **kwargs)
+        return wrapper
+
 
 def memoized(fn):
     pass
+
+class counters(object):
+    counter = 0
+
+
+def count_calls(fn):
+    @functools.wraps(fn)
+    def wrapper():
+        counters.counter += 1
+        fn()
+    return wrapper
